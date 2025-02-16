54bfe4123afb3307e5381a0954d5d9cd44d15f45
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-02-16 13:21:13 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,21 +1,90 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
+*_Song Down Number Reply Plugin...👇❤️_*
 
+/*
+Please Give Credit 🙂❤️
+⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚
+*/
+//=============================================
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+const yts = require('yt-search');
+const domain = `Manul-Official-Key-3467`;
+//===== Api Link එක මට Message එකක් දාල ඉල්ලගන්න, +94 74 227 4855 සල්ලි ගන්න නෙවේ, කීයක් Use කරනවද දැනගන්න...❤️=====
+//=============================================
 cmd({
-    pattern: "ai",
-    alias: ["gpt","bot"], 
-    react: "📑",
-    desc: "ai chat.",
-    category: "main",
+    pattern: "song",
+    alias: ["audio"],
+    desc: 'Download Song / Video',
+    use: '.play Title',
+    react: "🎧",
+    category: 'download',
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
-return reply(`${data.data}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q) return reply('Please provide a title.');
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        let desc = `*💚🎵 𝐘𝐓 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 🎵💚*
+        
+> *\`➤ Title\` :* ${data.title}
+
+> *\`➤ Views\` :* ${data.views}
+
+> *\`➤ DESCRIPTION\`:* ${data.description}
+
+> *\`➤ TIME\`:* ${data.timestamp}
+
+> *\`➤ AGO\`:* ${data.ago}
+
+*◄❪ Reply This Message With Nambars ❫►*
+
+1. Audio 🎧
+2. Document 🗂️
+
+> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚*`;
+
+        const vv = await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
+
+        conn.ev.on('messages.upsert', async (msgUpdate) => {
+            const msg = msgUpdate.messages[0];
+            if (!msg.message || !msg.message.extendedTextMessage) return;
+
+            const selectedOption = msg.message.extendedTextMessage.text.trim();
+
+            if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === vv.key.id) {
+                switch (selectedOption) {
+                    case '1':
+    const response = await fetchJson(`${domain}/ytmp3-fix?url=${data.url}`);
+    
+    const downloadUrl = response.dl_link;
+
+//============Send Audio======================
+await conn.sendMessage(from,{audio:{url: downloadUrl },mimetype:"audio/mpeg",caption :"> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚*"},{quoted:mek})
+                        break;
+       
+                    case '2':               
+const responsex = await fetchJson(`${domain}/ytmp3-fix?url=${data.url}`);
+    
+    const downloadUrlx = response.dl_link;
+
+//=============Send Document=================
+await conn.sendMessage(from,{document:{url: downloadUrlx },mimetype:"audio/mpeg",fileName: data.title + ".mp3" ,caption :"> *⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚*"},{quoted:mek})
+                        break;
+ 
+                    default:
+                        reply("Invalid option. Please select a valid option 💗");
+                }
+
+            }
+        });
+
+    } catch (e) {
+        console.error(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } })
+        reply('An error occurred while processing your request.');
+    }
+});
+//=============©𝐌𝐑 𝐌𝐀𝐍𝐔𝐋 𝐎𝐅𝐂 💚==========

a10f0f733e2c3db96ace86a5b9f749cbe7a9eeb0
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-02-19 23:12:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "KQ5HXYpY#HSWY1GDMzV7GVlwbvWvN-Rv7DlcICI7miDvYWiw3vYw",
+    SESSION_ID: process.env.SESSION_ID || "f7B2CRga#carKly99glBxjqyhmldjbe8GITyDKKLM-AqHRSGJmHQ",
     CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
     CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
     CAPTION: process.env.CAPTION || "*𝐐𝐔𝐄𝐄𝐍 𝐇𝐀𝐒𝐇𝐈 𝐌𝐃 𝐕2*",
